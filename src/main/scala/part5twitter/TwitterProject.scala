43b55d503cce806bc60cc1af2a83ec9548b9bd1d
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2020-03-06 16:34:27 +0200

    twitter exercises

--- src/main/scala/part5twitter/TwitterProject.scala
+++ src/main/scala/part5twitter/TwitterProject.scala
@@ -29,8 +29,42 @@ object TwitterProject {
     ssc.awaitTermination()
   }
 
+  /**
+    * Exercises
+    * 1. Compute average tweet length in the past 5 seconds, every 5 seconds. Use a window function.
+    * 2. Compute the most popular hashtags (hashtags most used) during a 1 minute window, update every 10 seconds.
+    */
+
+  def getAverageTweetLength(): DStream[Double] = {
+    val tweets = ssc.receiverStream(new TwitterReceiver)
+    tweets
+      .map(status => status.getText)
+      .map(text => text.length)
+      .map(len => (len, 1))
+      .reduceByWindow((tuple1, tuple2) => (tuple1._1 + tuple2._1, tuple1._2 + tuple2._2), Seconds(5), Seconds(5))
+      .map { megaTuple  =>
+        val tweetLengthSum = megaTuple._1
+        val tweetCount = megaTuple._2
+
+        tweetLengthSum * 1.0 / tweetCount
+      }
+  }
+
+  def computeMostPopularHashtags(): DStream[(String, Int)] = {
+    val tweets = ssc.receiverStream(new TwitterReceiver)
+
+    ssc.checkpoint("checkpoints")
+
+    tweets
+      .flatMap(_.getText.split(" "))
+      .filter(_.startsWith("#"))
+      .map(hashtag => (hashtag, 1))
+      .reduceByKeyAndWindow((x, y) => x + y, (x, y) => x - y, Seconds(60), Seconds(10))
+      .transform(rdd => rdd.sortBy(tuple => - tuple._2))
+  }
+
   def main(args: Array[String]): Unit = {
-    readTwitter()
+    computeMostPopularHashtags().print()
     ssc.start()
     ssc.awaitTermination()
   }
